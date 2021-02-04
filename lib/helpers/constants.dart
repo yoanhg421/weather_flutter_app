b71c85b1801c57535913667a2cddd8bc9a0bc0a7
yoanherrera <yoanhg421@gmail.com>
2021-02-03 17:06:22 -0800

    first commit

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kAPIKey = '6c14b683c3a5b889ec34102e9f8fb328';
 const kAPIURL = 'https://api.openweathermap.org/data/2.5/onecall';
 
 const TextStyle kWeatherBig = TextStyle(
